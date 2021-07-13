cebfb1765eaf6a6d4c87289f1af1af69c03c8f90
Divine Akinjiyan <ddacvq@mail.umsl.edu>
2021-07-13 21:08:54 +0000

    updated

--- templates/demo.py
+++ templates/demo.py
@@ -9,12 +9,27 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SECRET_KEY'] = ''
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(20), unique=True, nullable=False)
+  email = db.Column(db.String(120), unique=True, nullable=False)
+  password = db.Column(db.String(60), nullable=False)
+
+  def __repr__(self):
+    return f"User('{self.username}', '{self.email}')"
+  
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
-    if form.validate_on_submit(): # checks if entries are valid
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home')) # if so - send to home page
+    if form.validate_on_submit():
+      user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+      db.session.add(user)
+      db.session.commit()
+      flash(f'Account created for {form.username.data}!', 'success')
+      return redirect(url_for('home')) # if so - send to home page
     return render_template('register.html', title='Register', form=form)
   
 # secret key
